1e5f8c8efd25156b08d49d4ab56e2bd1f5f90609
unknown <aguerredev@gmail.com>
2020-05-01 19:50:27 -0300

    Added some test logging that will be deleted later and changed the starting point of view

--- Source/BuildingScape/WorldPosition.cpp
+++ Source/BuildingScape/WorldPosition.cpp
@@ -1,5 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "WorldPosition.h"
 
@@ -20,7 +20,9 @@ void UWorldPosition::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("This is a Warning!"));
+	UE_LOG(LogTemp, Error, TEXT("This is an Error!"));
+	UE_LOG(LogTemp, Display, TEXT("This is a Display Log!"));
 }
 
 
